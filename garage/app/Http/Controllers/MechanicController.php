6742884757f439fcc4137e3d39e9e724fe6b36ec
Aleksandr Vasiljev <zaswet@gmail.com>
2022-09-14 12:16:21 +0300

    garage

---
+++ garage/app/Http/Controllers/MechanicController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mechanic;
+use App\Http\Requests\StoreMechanicRequest;
+use App\Http\Requests\UpdateMechanicRequest;
+
+class MechanicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreMechanicRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMechanicRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Mechanic  $mechanic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mechanic $mechanic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Mechanic  $mechanic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mechanic $mechanic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateMechanicRequest  $request
+     * @param  \App\Models\Mechanic  $mechanic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMechanicRequest $request, Mechanic $mechanic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Mechanic  $mechanic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mechanic $mechanic)
+    {
+        //
+    }
+}
---
+++ garage/app/Http/Controllers/TruckController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Truck;
+use App\Http\Requests\StoreTruckRequest;
+use App\Http\Requests\UpdateTruckRequest;
+
+class TruckController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTruckRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTruckRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Truck  $truck
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Truck $truck)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Truck  $truck
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Truck $truck)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTruckRequest  $request
+     * @param  \App\Models\Truck  $truck
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTruckRequest $request, Truck $truck)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Truck  $truck
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Truck $truck)
+    {
+        //
+    }
+}
